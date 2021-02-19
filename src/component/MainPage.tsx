b197e83b5ba02e84a4ca7842e1ca0fcfad2f8f3b
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-02-19 23:24:43 +0900

    add lib

--- src/component/MainPage.tsx
+++ src/component/MainPage.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 
-const App: React.FC<Props> = (props) => {
+const MainPage: React.FC<Props> = (props) => {
   return (
     <div>
-
+      
     </div>
   );
 }
 type Props = void;
-export default App;
+export default MainPage;
